2d518d71770b665aba93d51d09155e295bd4540d
Ajoy Sarkar (Anondo) <ajoysarker553@gmail.com>
2024-03-06 18:00:00 +0600

    commit

--- src/compontes/BookMark/BookMark.jsx
+++ src/compontes/BookMark/BookMark.jsx
@@ -3,11 +3,26 @@ import PropTypes from "prop-types";
 const BookMark = ({ bookmark }) => {
 	const { title } = bookmark;
 	return (
-		<div className="bg-yellow-100 p-4 m-4">
+		<div className="p-4 m-4 bg-yellow-100">
 			<h3>{title}</h3>
 		</div>
 	);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 BookMark.propTypes = {
 	bookmark: PropTypes.object,
